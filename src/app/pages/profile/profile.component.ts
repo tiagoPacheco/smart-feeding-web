c5f9bc9e909f88914ce361c19383af991f06efa5
tiagoPacheco <tiagoh.gpacheco@gmail.com>
2018-09-01 14:52:33 -0300

    adjustment

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationModel } from '../../shared/models/notification-model';
+import { GlobalService } from '../../shared/services/global.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,8 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _globalService: GlobalService) { }
 
   ngOnInit() { }
 
+  updateProfile(){
+    this.alertMessage(
+      {
+        type: 'default',
+        title: 'Look here!',
+        value: 'updateProfile.'
+      }
+    );
+  }
+
+  updateStock(){
+    this.alertMessage(
+      {
+        type: 'default',
+        title: 'Look here!',
+        value: 'updateStock.'
+      }
+    );
+  }
+
+  updateSettings(){
+    this.alertMessage(
+      {
+        type: 'default',
+        title: 'Look here!',
+        value: 'updateSettings.'
+      }
+    );
+  }
+
+  alertMessage(data: NotificationModel = {
+    type: 'default',
+    title: 'Look here!',
+    value: 'This alert needs your attention.'
+  }) {
+      //this._globalService._notification(data);
+      this._globalService.dataBusChanged('notification', data);
+  }
 }
